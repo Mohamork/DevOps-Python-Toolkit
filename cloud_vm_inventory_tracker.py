d1af6b9c8eac7f2d0e122e4870eee5f4ffdaa798
Mohamork <k.m19981102@gmail.com>
2026-04-04 16:06:16 +0200

    First commit from local repo

---
+++ cloud_vm_inventory_tracker.py
@@ -0,0 +1,65 @@
+vms = {
+    'vm1': {
+        'ip': '10.10.0.1',
+        'status': 'running'
+        },
+    'vm2': {
+        'ip' : '10.10.0.2',
+        'status': 'stopped'
+        }, 
+    'vm3': {
+        'ip': '10.10.0.3',
+        'status': 'running'
+        },
+    'vm4': {
+        'ip' : '10.10.0.4',
+        'status': 'stopped'
+        },
+    'vm5': {
+        'ip': '10.10.0.5',
+        'status': 'running'
+        },     
+}
+
+
+running_vms = []
+stopped_vms = []
+
+for vm,info in vms.items():
+
+    host = vm
+    ip = info['ip']
+    status = info['status']
+
+    if status == 'running':
+        
+        message = f'Host: {host}\tip: {ip}\tstatus: {status}\n'
+
+        running_vms.append(message)
+
+    elif status == 'stopped':
+
+        message = f'Host:{host}\tip: {ip}\tstatus: {status}\n'
+
+        stopped_vms.append(message)
+
+
+for vm_status in running_vms:
+
+    print(vm_status)
+
+count = len(running_vms)
+print('Running vm count:',count)
+print()
+
+for vm_status in stopped_vms:
+
+    print(vm_status)
+
+count = len(stopped_vms)
+
+print('Stopped vm count:',count)
+
+
+
+    
---
+++ port_scan_simulator.py
@@ -0,0 +1,52 @@
+import socket
+from contextlib import closing
+from datetime import datetime
+
+def check_port(host,port,timeout=2):
+
+    with closing(socket.socket(socket.AF_INET,socket.SOCK_STREAM)) as sock:
+
+        sock.settimeout(timeout)
+
+        if sock.connect_ex((host,port)) == 0:
+
+            now = datetime.now()
+
+            result = f'{now}    Port {port} on host {host} is OPEN'
+
+            return result
+        
+        else:
+
+            now = datetime.now()
+
+            result = f'{now}    Port {port} on host {host} is CLOSED'
+
+            return result
+        
+ip_addresses = []
+user_input = input('Please enter an IP address > ')
+
+user_input = user_input.split(',')
+for ip in user_input:
+
+    ip_addresses.append(ip.strip())
+
+user_input= input('Please enter a portnumber > ')
+port = int(user_input)
+
+results = []
+for ip in ip_addresses:
+
+    results.append(check_port(ip,port))
+
+for result in results:
+
+    print(result)
+    print()
+
+    
+
+
+
+
