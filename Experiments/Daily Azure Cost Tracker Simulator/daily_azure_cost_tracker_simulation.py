170bf4742b408910e4aeacad286db7f65e33b7f9
Mohamork <k.m19981102@gmail.com>
2026-04-15 16:21:54 +0200

    Create new experiement for azure cost analysis

---
+++ Experiments/Daily Azure Cost Tracker Simulator/daily_azure_cost_tracker_simulation.py
@@ -0,0 +1,31 @@
+import csv 
+
+filename = 'costs.csv'
+
+with open(filename,'r') as f:
+
+    reader = csv.reader(f)
+    data = list(reader)
+    headers = data[0]
+    
+output =  ['Date','ServiceName','Cost']
+
+
+for header in headers:
+
+    if header in output:
+
+        print(header,end='\t\t')
+print()
+for line in data[1:4]:
+
+    cost = line[4]
+    servicename=line[1]
+    date=line[0]
+    message = f'{date}\t{servicename:20}\t{cost}'
+
+    print(message)
+
+    
+    
+
